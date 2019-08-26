c022d857d3aaccdf97551c6726e534ef87defc35
Cameron Gorrie <sastraxi@gmail.com>
2019-08-25 23:03:47 -0400

    fix some bad code

--- src/light/gpio.h
+++ src/light/gpio.h
@@ -7,16 +7,17 @@
   #define GPIO_PWM_RANGE 255
   #define SET_PWM_RANGE_AT_INIT
 
-  const int[][] GPIO_PINS = [
+  const int** GPIO_PINS = [
     [2, 3, 4]
   ];
 
   void __light_init(void) {
     gpioInitialise();
     #ifdef SET_PWM_RANGE_AT_INIT
-      for (int pin = 0; pin < len(GPIO_PINS); ++pin) {
+      const int pinCount = sizeof(GPIO_PINS) / sizeof(GPIO_PINS[0]);
+      for (int pin = 0; pin < pinCount; ++pin) {
         for (int v = 0; v < 3; ++v) {
-          gpioSetPWMRange(GPIO_PINS[pin][v], GPIO_PWM_RANGE);
+          gpioSetPWMrange(GPIO_PINS[pin][v], GPIO_PWM_RANGE);
         }
       }
     #endif
@@ -27,10 +28,10 @@
   }
   
   inline int setLight(int pin, float red, float green, float blue) {
-    const float[] colour = [red, green, blue];
+    const float* colour = [red, green, blue];
     for (int v = 0; v < 3; ++v) {
       int rv = gpioPWM(GPIO_PINS[pin][v], colour[v]);
-      if (rv !== 0) return rv;
+      if (rv != 0) return rv;
     }
     return 0;
   }
