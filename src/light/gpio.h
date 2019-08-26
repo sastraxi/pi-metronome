512e918daa73c2108436259aa95da901d2481f51
Cameron Gorrie <sastraxi@gmail.com>
2019-08-25 23:08:27 -0400

    another fix for arrays?

--- src/light/gpio.h
+++ src/light/gpio.h
@@ -7,7 +7,7 @@
   #define GPIO_PWM_RANGE 255
   #define SET_PWM_RANGE_AT_INIT
 
-  const int[][3] GPIO_PINS = {
+  const int** GPIO_PINS = {
     {2, 3, 4}
   };
 
@@ -28,7 +28,7 @@
   }
   
   inline int setLight(int pin, float red, float green, float blue) {
-    const float[3] colour = {red, green, blue};
+    const float* colour = {red, green, blue};
     for (int v = 0; v < 3; ++v) {
       int rv = gpioPWM(GPIO_PINS[pin][v], colour[v]);
       if (rv != 0) return rv;
