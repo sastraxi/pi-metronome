8c8363c15be72b8869a525e4f85f3bd059d7188b
Cameron Gorrie <sastraxi@gmail.com>
2019-09-01 11:56:50 -0400

    use pigpio for timing rather than asm

--- src/bpm.c
+++ src/bpm.c
@@ -9,14 +9,12 @@
 
 #include "light/gpio.h"
 
-#define MMIO_BASE       0x3F000000
-#define SYSTMR_LO       ((volatile unsigned int*)(MMIO_BASE+0x00003004))
-#define SYSTMR_HI       ((volatile unsigned int*)(MMIO_BASE+0x00003008))
-
 #define M_PI 3.14159265358979323846
 
 #define SEC_TO_MICROSEC 1000000
 #define MICROSEC_TO_SEC 0.000001
+
+const double INV_UPDATE_HZ = 1.0 / 2000.0;
 
 const int NUM_LIGHTS  = 4;
 const double INV_NUM_LIGHTS = 1.0 / (double) NUM_LIGHTS;
@@ -28,38 +26,17 @@ inline double max(double a, double b) {
   return b;
 }
 
-/**
- * Wait N microsec (ARM CPU only)
- * From https://github.com/bztsrc/raspi3-tutorial/blob/master/07_delays/delays.c#L39
- */
-void wait_microsec(unsigned int n)
+int wait_microsec(unsigned int n)
 {
-    register unsigned long f, t, r;
-    // get the current counter frequency
-    asm volatile ("mrs %0, cntfrq_el0" : "=r"(f));
-    // read the current counter
-    asm volatile ("mrs %0, cntpct_el0" : "=r"(t));
-    // calculate expire value for counter
-    t+=((f/1000)*n)/1000;
-    do{asm volatile ("mrs %0, cntpct_el0" : "=r"(r));}while(r<t);
+  return gpioSleep(PI_TIME_RELATIVE, 0, n);
 }
 
-/**
- * Get System Timer's counter
- */
 unsigned long get_system_timer()
 {
-    unsigned int h=-1, l;
-    // we must read MMIO area as two separate 32 bit reads
-    h=*SYSTMR_HI;
-    l=*SYSTMR_LO;
-    // we have to repeat it if high word changed during read
-    if(h!=*SYSTMR_HI) {
-        h=*SYSTMR_HI;
-        l=*SYSTMR_LO;
-    }
-    // compose long int value
-    return ((unsigned long) h << 32) | l;
+  int sec, microsec;
+  gpioTime(PI_TIME_RELATIVE, &sec, &microsec);
+  unsigned long systime = sec * SEC_TO_MICROSEC + microsec;
+  return systime;
 }
 
 /**
@@ -82,7 +59,7 @@ int main(int argc, char** argv)
 
   const double bpm = (float) atoi(argv[1]);
   const double hz = 1.0 / (bpm / 60.0);
-  const unsigned int sleep_time = SEC_TO_MICROSEC * hz;
+  const unsigned int sleep_time = SEC_TO_MICROSEC * INV_UPDATE_HZ;
 
   __light_init();
   setLightRGB(0, 0.0f, 0.0f, 0.0f);
