8f5e686b42e3e994efa1b8a566448c4e6aa69b92
Cameron Gorrie <cameron.gorrie@tophatmonocle.com>
2019-09-08 22:08:27 -0400

    get things working on ios

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -4,11 +4,27 @@ import 'package:flutter_blue/flutter_blue.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  MyApp() {
+    FlutterBlue flutterBlue = FlutterBlue.instance;
+    /// Start scanning
+    try {
+      var scanSubscription = flutterBlue.scan().listen((scanResult) {
+        // do something with scan result
+        var device = scanResult.device;
+        print('${device.name} found! rssi: ${scanResult.rssi}');
+      });
+      scanSubscription.cancel();
+    } on Exception catch (err) {
+      print(err);
+    }
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Visual Metronome',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -19,24 +35,15 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Visual Metronome'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key) {
-    FlutterBlue flutterBlue = FlutterBlue.instance;
-    /// Start scanning
-    var scanSubscription = flutterBlue.scan().listen((scanResult) {
-      // do something with scan result
-      var device = scanResult.device;
-      print('${device.name} found! rssi: ${scanResult.rssi}');
-    });
-    scanSubscription.cancel();
-  }
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -69,36 +76,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.bluetooth),
+            color: Colors.white,
+          )
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
           // Invoke "debug painting" (press "p" in the console, choose the
           // "Toggle Debug Paint" action from the Flutter Inspector in Android
           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
           // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -115,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
